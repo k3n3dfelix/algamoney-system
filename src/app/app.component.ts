c9f938e745b85a8add2d32995c24a143486ceede
Kened Silva Felix <ksfelix1@stefanini.com>
2022-03-30 11:42:18 -0300

    Criado coluna Opções e adicionado paginação na tabela principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
       tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/04/2022', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Jóse'
     },
     {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/04/2022', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do Jóse'
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/04/2022', dataPagamento: null, valor: 4.56, pessoa: 'Padaria do Jóse'
     },
   ];
 }
